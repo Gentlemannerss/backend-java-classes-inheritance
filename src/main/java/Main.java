7c2fad718fea76c155037cc2397b9830de6863c8
Damon <damonvanbergen@gmail.com>
2023-03-07 13:48:00 +0100

    huiswerk stap1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Customer customer = new Customer("damon");
+        customer.setName("Damon");
+        System.out.println(customer.name);
     }
 }
